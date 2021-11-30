5f670a1b8be52a64dcae9592f490d717bb3a05a3
uvuvumuemue <6tongranituikrzemowykwas@gmail.com>
2021-11-30 22:09:59 +0100

    sortowanie 2.0

---
+++ PracaNaLekcjiNr3/ConsoleApplication3/ConsoleApplication3/ConsoleApplication3.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include<vector>
+#include"sortowanie.h"
+using namespace std;
+int main() {
+        sortowanie Liczby;
+        Liczby.generator();
+        char wybor;
+        cout << "\nwybierz sortowanie\n1-bombelkowe\n2-wstawieniowe\n";
+        cin >> wybor;
+        if (wybor == '1') {
+            Liczby.bombelkowy();
+        }
+        else if (wybor == '2') {
+            Liczby.wstawianie();
+        }
+        else {
+            cout << "\nzle wpisales, moze nastepnym razem ci sie uda\n";
+        }
+}
---
+++ PracaNaLekcjiNr3/ConsoleApplication3/ConsoleApplication3/sortowanie.h
@@ -0,0 +1,13 @@
+#include<vector>
+using namespace std;
+class sortowanie
+{
+public:
+	void wyswietlanie();
+	void generator();
+	void bombelkowy();
+	void wstawianie();
+private:
+	int x, i;
+	vector <int> liczby_wylosowane;
+};
